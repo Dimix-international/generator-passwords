a5eb1ea3e08bcea1b5a3dd99e9ad6cf7e7f98d71
Dimix-international <dima.dimix94@mail.ru>
2022-06-20 11:23:11 +0300

    kanban

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
 import {Kanban} from "./graggableKanban/Kanban";
-import {GeneratorComponent} from "./generatorPassword/GeneratorComponent";
 
 function App() {
 
   return (
     <div className="App">
-        <GeneratorComponent />
         <Kanban />
     </div>
   );
